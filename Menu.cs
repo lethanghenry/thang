7d2c2a023a32990ae95ba0fca3734fcf52b94a3f
Lê Văn Thắng <87084962+lethanghenry@users.noreply.github.com>
2021-10-19 20:05:35 +0700

    bài cuối ngày 19/10/2021

---
+++ Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Menu
+    {
+        public string title { get; set; }
+        public string id { get; set; }
+        public string parent_id { get; set; }
+
+    }
+}
